b8c0aace178fb4b3f523343a83333f7f02e7e65a
Nannings <mrnannings@gmail.com>
2018-01-27 05:03:11 +0100

    newgame;

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+    public GameObject com;
+    public Vector3 direction;
+
+    public Color pressed;
+    public Color notPressed;
+
+    bool isOver;
+
+    private float moveSpeed = 20;
+    private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start ()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (isOver && Input.GetMouseButton(0))
+        {
+            com.transform.Rotate(direction * moveSpeed * Time.deltaTime);
+            spriteRenderer.color = pressed;
+        }
+        else
+        {
+            spriteRenderer.color = notPressed;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Finger"))
+        {
+            isOver = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Finger"))
+        {
+            isOver = false;
+        }
+    }
+}
---
+++ Assets/Scripts/SatCom.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SatCom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float angle = Quaternion.Angle(transform.rotation, Quaternion.identity);
+        if (angle < 10)
+        {
+            SceneManager.LoadScene("level2");
+        }
+
+        Debug.Log(angle);
+	}
+}
