0eb9c20d523a82a4e9efa614ebbb004157216745
Nannings <mrnannings@gmail.com>
2018-01-27 10:34:56 +0100

    update;

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+
+    public GameObject com;
+    public Vector3 direction;
+    public Color pressed;
+    public Color notPressed;
+    public bool hasDelay;
+
+    private bool isOver;
+    private float moveSpeed = 20;
+    private SpriteRenderer spriteRenderer;
+    private float releaseDelay = 0;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        if (isOver && Input.GetMouseButton(0))
+        {
+            moveSat();
+            releaseDelay = 10;
+        }
+        else
+        {
+            release();
+            spriteRenderer.color = notPressed;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Finger"))
+        {
+            isOver = true;
+        }
+        if (collision.CompareTag("SuctionCup"))
+        {
+            hasDelay = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Finger"))
+        {
+            isOver = false;
+        }
+    }
+
+    private void release()
+    {
+        if (releaseDelay > 0 && hasDelay)
+        {
+            releaseDelay -= Time.deltaTime * 10;
+            moveSat();
+        }
+        else
+        {
+            releaseDelay = 0;
+        }
+    }
+
+    private void moveSat()
+    {
+        com.transform.Rotate(direction * moveSpeed * Time.deltaTime);
+        spriteRenderer.color = pressed;
+    }
+}
