69e4d4c1c51864fb2268a59c5ae52e9bad0a21c3
HLA-systeem <aaakanten@outlook.com>
2018-01-27 15:17:59 +0100

    commit for sync

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -63,7 +63,7 @@ public class Button : MonoBehaviour
         {
             releaseDelay -= Time.deltaTime * 10;
             moveSat();
-            spriteRenderer.color = notPressed;
+            spriteRenderer.color = pressed;
         }
         else
         {
