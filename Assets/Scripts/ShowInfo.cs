c5c0c412c3cdda31a08c7ccff37352010235e206
HLA-systeem <aaakanten@outlook.com>
2018-01-28 10:53:26 +0100

    finaltouches

--- Assets/Scripts/ShowInfo.cs
+++ Assets/Scripts/ShowInfo.cs
@@ -1,21 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ShowInfo : MonoBehaviour {
 
     public GameObject popup;
+    private float popArea;
 
-	
-	void Start () {
-	}
+    void Start ()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 4 || SceneManager.GetActiveScene().buildIndex == 5)
+        {
+            popArea = Screen.width - (Screen.width / 3.5f);
+
+        }
+        else if(SceneManager.GetActiveScene().buildIndex == 7)
+        {
+            popArea = Screen.width - (Screen.width / 3);
+        }
+        else if (SceneManager.GetActiveScene().buildIndex == 6)
+        {
+            popArea = Screen.width - (Screen.width / 2.5f);
+        }
+        else {
+            popArea = Screen.width - (Screen.width / 4);
+        }
+    }
 	
 	void Update () {
         //Debug.Log(mousePosition);
         //if (mousePosition.x > 5.4){
 
         //}
-        float popArea = Screen.width - (Screen.width / 3);
+
         popup.SetActive(Input.mousePosition.x >= popArea);
     }
 }
