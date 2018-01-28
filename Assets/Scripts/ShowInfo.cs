f4ec8c67705f9ec46905a8e99a1cc59e68424cde
HLA-systeem <aaakanten@outlook.com>
2018-01-28 09:59:23 +0100

    helpmenu again adn popup

---
+++ Assets/Scripts/ShowInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowInfo : MonoBehaviour {
+
+    public GameObject popup;
+
+	
+	void Start () {
+	}
+	
+	void Update () {
+        //Debug.Log(mousePosition);
+        //if (mousePosition.x > 5.4){
+
+        //}
+        float popArea = Screen.width - (Screen.width / 3);
+        popup.SetActive(Input.mousePosition.x >= popArea);
+    }
+}
